324e1b20342ab038471071f125fb0300594a8aa9
Yauheniy Tyshevich <etyshevich@bogorad.studio>
2023-10-05 08:38:22 +0300

    fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,7 @@
 const Header = () => {
-  // const user = "state.user";
-
   return (
     <header className="nft_prices-header" data-testid="header">
       <div>Developer Challenge</div>
-      {/* {user.email && <div className="blackshield-avatar">{user.email}</div>} */}
     </header>
   );
 };
